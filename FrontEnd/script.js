7f3794bf638cba3e334e33de732677cbe49b3a50
Jérémie <jeremie_marie14@hotmail.fr>
2024-10-29 14:58:53 +0100

    ADD: fetch api data for works ADD: generate works part ADD: filtering logic on buttons

---
+++ FrontEnd/script.js
@@ -0,0 +1,20 @@
+import {generateWorks} from "./works.js";
+import { initializeFilterButtons } from './buttons.js';
+
+let works = window.localStorage.getItem("works");
+
+if (!works){
+    const answer = await fetch("http://localhost:5678/api/works");
+    works = await answer.json();
+
+    const valueWorks = JSON.stringify(works);
+    window.localStorage.setItem("works", valueWorks);
+} else {
+    works = JSON.parse(works);
+}
+
+generateWorks(works);
+initializeFilterButtons();
+
+
+
