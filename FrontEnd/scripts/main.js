d7d65f1b54073a4e39700ee0b476dc7d61a35923
Jérémie Marie <jeremie_marie14@hotmail.fr>
2024-11-21 16:24:07 +0100

    RM: import for sendcontactform since it's not asked to implement, eventlistener removed too MODIF: loadworks import path to the new apiservice.js file

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -1,8 +1,7 @@
 import { generateWorks } from "./features/works/works.js";
-import { loadWorks } from './features/works/loadWorks.js';
+import { loadWorks } from './services/apiService.js';
 import { initializeFilterButtons } from './components/filterButtons.js';
 import { displayEditionMode, updateLoginLogoutButton, displayEditButton } from './features/admin/displayAdmin.js';
-import { sendContactForm } from './features/contact/contact.js';
 
 const works = await loadWorks(); // Chargement des works
 generateWorks(works); // Affichage des travaux dans la galerie principale
@@ -11,4 +10,3 @@ initializeFilterButtons(); // Initialisation des boutons de filtre
 document.addEventListener("DOMContentLoaded", updateLoginLogoutButton); // Affichage du bouton Login/Logout selon l'état de la connexion
 document.addEventListener('DOMContentLoaded', displayEditionMode); // Affichage du container edition-mode selon l'état de la connexion
 document.addEventListener('DOMContentLoaded', displayEditButton); // Affichage du bouton Modifier selon l'état de la connexion
-document.addEventListener('DOMContentLoaded', sendContactForm); // Envoi du formulaire de contact (TODO: Ajouter la fonction POST)
