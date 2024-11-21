513b2a68e18eaf828244e2156da89b75a7324206
Jérémie Marie <jeremie_marie14@hotmail.fr>
2024-11-21 17:16:59 +0100

    ADD: config.js file that contains API_CONFIG informations with :
    BASE URL
    ENDPOINTS
    
    To facilitate prod env

---
+++ FrontEnd/scripts/services/config.js
@@ -0,0 +1,10 @@
+const API_CONFIG = {
+  BASE_URL: 'http://localhost:5678/api',
+  ENDPOINTS: {
+      WORKS: '/works',
+      LOGIN: '/users/login',
+      CATEGORIES: '/categories'
+  }
+};
+
+export default API_CONFIG;
